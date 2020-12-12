bbbb038f22ad4fc21ce6981acd517955dc71a7e1
Baassza <oppopopios@gmail.com>
2020-12-12 17:44:45 +0700

    Initial commit

---
+++ src/io/kodular/oppopopios/chatOK/chat.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["c.kodular.io"],"YaVersion":"210","Source":"Form","Properties":{"$Name":"chat","$Type":"Form","$Version":"41","AppId":"5481619181797376","AppName":"chatOK","BackgroundImage":"bg_ok.png","ReceiveSharedText":"none","ShowOptionsMenu":"False","ShowStatusBar":"False","Title":"chat","TitleVisible":"False","Uuid":"0","$Components":[{"$Name":"Web_Viewer1","$Type":"WebViewer","$Version":"13","Height":"-2","Width":"-2","Uuid":"1651069279"},{"$Name":"logout","$Type":"Button","$Version":"11","BackgroundColor":"&H00FFFFFF","Height":"50","Width":"110","Uuid":"1182470101"},{"$Name":"Tiny_DB1","$Type":"TinyDB","$Version":"2","Uuid":"2026561790"},{"$Name":"Notifier1","$Type":"Notifier","$Version":"10","Uuid":"-1353479053"}]}}
+|#
